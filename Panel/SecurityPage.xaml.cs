20c3d3724425f1f7bf487566fe0b6f1b53a311e8
Laksh <gamer.565699@gmail.com>
2026-02-08 14:19:35 +0530

    Add project files.

---
+++ Panel/SecurityPage.xaml.cs
@@ -0,0 +1,40 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Test.Services;
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Test.Panel
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SecurityPage : Page
+    {
+        public SecurityPage()
+        {
+            this.InitializeComponent();
+
+        }
+        private void Dashboard_click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(DashboardPage));
+        }
+        private void Devices_click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(DevicesPage));
+        }
+        private void Protection_click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(ProtectionPage));
+        }
+        private void WebSecurity_click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(WebSecurityPage));
+        }
+        private void Settings_click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate (typeof(SettingsPage));
+        }
+    }
+}
